284d42d4e413ccd61ea68ce7a13ecdc6124653ed
Farhad hsni <farhad.hsni@gmail.com>
2016-12-09 17:38:05 +0330

    Improve format to value units after numbers

--- src/scripts/format.js
+++ src/scripts/format.js
@@ -1,5 +1,17 @@
 export default function format(mathFormat, ans) {
   const result = mathFormat(ans, { notation: "fixed", precision: 4 });
+  let nonNumberPart;
+  let numberPart = result.replace(/\s\w*\s*$/i, (match) => {
+    // if there's any word after numbers like units
+    nonNumberPart = match;
 
-  return result.replace(/0+$/, "").replace(/\.$/, ""); // removing extra zeros at the end
+    return "";
+  });
+
+  numberPart = numberPart.replace(/0+$/, "").replace(/\.$/, ""); // removing extra zeros at the end
+  if (nonNumberPart !== undefined) { // in case there wasn't any unit/word
+    return `${numberPart}${nonNumberPart}`;
+  }
+
+  return numberPart;
 }
