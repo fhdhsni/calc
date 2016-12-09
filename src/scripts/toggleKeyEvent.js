17d13d61450ce3246e8f203fb28eceb884aa82f3
Farhad hsni <farhad.hsni@gmail.com>
2016-12-09 18:30:16 +0330

    fix height of input on hidden-keys

--- src/scripts/toggleKeyEvent.js
+++ src/scripts/toggleKeyEvent.js
@@ -1,8 +1,12 @@
 export default function (toggleKeys, resultWrapper, keyContainer) {
+  const inputWrapper = document.querySelector(".input-wrapper");
+
   toggleKeys.addEventListener("click", function () {
     const flag = keyContainer.classList.toggle("display-none");
 
     resultWrapper.classList.toggle("on-hidden-keys");
+    inputWrapper.classList.toggle("on-hidden-keys");
+
     if (flag) {
       this.textContent = "show keys";
     } else {
