2dd8fd2412b1472d57cfaea6bf3ffade11dc4096
Farhad hsni <farhad.hsni@gmail.com>
2016-12-19 03:32:32 +0330

    Refactor to use helper function concatToValue

--- src/scripts/addEventsForSameContentKeys.js
+++ src/scripts/addEventsForSameContentKeys.js
@@ -1,10 +1,10 @@
 /* eslint no-param-reassign: ["error", { "props": false }]*/
+import concatToValue from "./concatToValue";
+
 export default function addEvents() {
   for (let i = 0; i < this.sameText.length; i += 1) {
     const key = this.sameText[i];
 
-    key.addEventListener("click", () => {
-      this.input.value += key.textContent;
-    });
+    key.addEventListener("click", () => concatToValue(this.input, key.textContent));
   }
 }
