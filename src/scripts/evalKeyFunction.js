dde23093646ade1d4ab8642b0d9ebf91087eb8bd
Farhad hsni <farhad.hsni@gmail.com>
2017-01-05 14:56:04 +0330

    Refactor for the sake of easier testing

--- src/scripts/evalKeyFunction.js
+++ src/scripts/evalKeyFunction.js
@@ -1,27 +1,20 @@
 /* eslint no-param-reassign: ["error", { "props": false }]*/
-// import math from "./math.min";
-import math from "mathjs";
-import showHelp from "./showHelp";
-import closeHelp from "./closeHelp";
-import buildResultElement from "./buildlResultElement";
-import format from "./format";
-import evt from "./buildCustomEvent";
 
 export default function evalKeyFunction() {
-  closeHelp.call(this);
+  this.closeHelp();
   this.evalKey.addEventListener("click", () => {
     const mathExpression = this.input.value.trim();
 
     if (mathExpression !== "") {
       if (mathExpression.search(/help/) === -1) {
-        const resultValue = format(math.format, math.eval(mathExpression));
+        const resultValue = this.format(this.math.format, this.math.eval(mathExpression));
 
-        buildResultElement.call(this, mathExpression, { resultValue });
-        document.dispatchEvent(evt);
+        this.buildResultElement(mathExpression, { resultValue });
+        document.dispatchEvent(this.buildCustomEvent);
       } else {
-        const result = math.eval(mathExpression);
+        const result = this.math.eval(mathExpression);
 
-        showHelp.call(this, result.doc);
+        this.showHelp(result.doc);
         this.helpWrapper.classList.add("on-help");
         this.helpWrapper.tabIndex = "-1";  // to make it focusable
         this.helpWrapper.focus();
